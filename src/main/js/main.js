6134ac328d1af09eb0deb56400261dc424d41036
Max Bruchmann <bruchmann@signal7.de>
2015-03-03 01:12:22 +0800

    initial commit

---
+++ src/main/js/main.js
@@ -0,0 +1,8 @@
+goog.provide("silksmith.page");
+
+silksmith.page = function(){
+
+  $('<div class="alert alert-success" role="alert"></div>').appendTo("body").text("something with jquery....yey!");
+};
+
+silksmith.page();
